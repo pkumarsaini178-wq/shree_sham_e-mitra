e9251efc25a8742fd29e86eed2b9ee70af95d33c
pankaj saini <java71932@gmail.com>
2026-06-25 21:19:40 -0700

    added vercel config

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,4 @@
 import '../styles.css';
-import '@fortawesome/fontawesome-free/css/all.min.css';
-import Script from 'next/script';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 
@@ -14,12 +12,12 @@ export default function RootLayout({ children }) {
     <html lang="hi">
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
       </head>
       <body>
         <Navbar />
         {children}
         <Footer />
-        <Script src="https://kit.fontawesome.com/your-font-awesome-kit.js" crossOrigin="anonymous" strategy="lazyOnload" />
       </body>
     </html>
   );
